491e2de4acbb297adae91a1c3f9635a833e0cefe
Sanam Malik <sanam.malik04@gmail.com>
2020-06-25 00:07:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  def update_counting_hash(hash, key)
   if hash[key]
-else
-end
+    hash[key] = hash[key] + 1
+  else
+    hash[key] = 1
+  end
+  hash
 end
